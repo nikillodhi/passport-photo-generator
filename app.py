32d62b6b8acd206566a0b0c9e7ea19eacc89a9a1
Nikil Lodhi <lodhinikil163@gmail.com>
2026-07-15 18:48:35 +0530

    Fix os import and indentation

--- app.py
+++ app.py
@@ -149,6 +149,4 @@ def process():
 
 
 if __name__ == "__main__":
-  import os
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
